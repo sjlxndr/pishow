5bc130d38687a602e118f72e9c707aeeffaff997
Stephen Alexander <sjalexander@das-rfp.com>
2025-09-07 13:58:05 -0500

    switch out xinit for startx

--- autostart.sh
+++ autostart.sh
@@ -2,8 +2,5 @@
 
 if [ "$SSH_CONNECTION" != "" ]; then exit; fi
 
-XDG_VTNR=1
-DISPLAY=:0
-
 # The single command to start the graphical session and run the slideshow manager
-xinit /home/pi/slideshow_manager.sh
+startx /home/pi/slideshow_manager.sh
