351b919bf41faa76e1478ccfa1b88364ee081888
Stephen Alexander <sj@lxndr.net>
2025-09-07 14:07:37 -0400

    cleaned up setup to avoid installing systemd-sysv (for startx)

--- autostart.sh
+++ autostart.sh
@@ -2,26 +2,8 @@
 
 if [ "$SSH_CONNECTION" != "" ]; then exit; fi
 
-# Define the mount point
-MOUNT_POINT="/mnt/slideshow"
-
-# Find the device to mount
-PART_NAME=$(lsblk --pairs -no NAME,TYPE -I 8 | awk '/TYPE="part"/ { print $1 }' | sed 's/NAME="//;s/"$//')
-DISK_NAME=$(lsblk --pairs -no NAME,TYPE -I 8 | awk '/TYPE="disk"/ { print $1 }' | sed 's/NAME="//;s/"$//')
-
-if [ -n "$PART_NAME" ]; then
-    DEVICE_TO_MOUNT="$PART_NAME"
-elif [ -n "$DISK_NAME" ]; then
-    DEVICE_TO_MOUNT="$DISK_NAME"
-fi
-
-# Check if a device was found and mount it
-if [ -n "$DEVICE_TO_MOUNT" ]; then
-    if [ ! -d "$MOUNT_POINT" ]; then
-        sudo mkdir -p "$MOUNT_POINT"
-    fi
-    sudo mount "/dev/$DEVICE_TO_MOUNT" "$MOUNT_POINT"
-fi
+XDG_VTNR=1
+DISPLAY=:0
 
 # The single command to start the graphical session and run the slideshow manager
-startx /home/pi/slideshow_manager.sh
+xinit /home/pi/slideshow_manager.sh
